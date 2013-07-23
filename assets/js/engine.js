e377a41ff5b1c29cd9bb0e4f2736eb7534b43666
Felipe Leão <felipe@felipeleao.com>
2013-07-23 18:25:34 -0300

    jsLint

--- assets/js/engine.js
+++ assets/js/engine.js
@@ -1,35 +1,35 @@
-(function(){
+(function () {
 
-	// Define as variáveis
-	var opt = 0,
-		valorTopo = $('#menu').offset().top,
-		valorAncora,
-		linkMenu;
+    'use strict';
 
-	//O Holder do Menu recebe a mesma altura que o Menu para evitar uma quebra da página
-	$('#holderMenu').height($('#menu').height());
-	
+    // Define as variáveis
+    var valorTopo = $('#menu').offset().top,
+        valorAncora,
+        linkMenu;
 
-	//Mantém o menu sempre no topo
-	$(window).scroll(function(){
-		if($(this).scrollTop() > valorTopo){
-			$('#menu').addClass('menuOn');
-		}else{
-			$('#menu').removeClass('menuOn');
-		}
-		return false;
-	});
+    //O Holder do Menu recebe a mesma altura que o Menu para evitar uma quebra da página
+    $('#holderMenu').height($('#menu').height());
 
-
-	// Clique nas âncoras
-	$('#menu > li > a').click(function(e){
-		e.preventDefault();
-		linkMenu = $(this).attr('href');
-		valorAncora = ($(linkMenu).offset().top) - 70;
-		jQuery('html, body').animate({scrollTop:valorAncora}, 'slow');
-	})
+    //Mantém o menu sempre no topo
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > valorTopo) {
+            $('#menu').addClass('menuOn');
+        } else {
+            $('#menu').removeClass('menuOn');
+        }
+        return false;
+    });
 
 
-	return false;
+    // Clique nas âncoras
+    $('#menu > li > a').click(function (e) {
+        e.preventDefault();
+        linkMenu = $(this).attr('href');
+        valorAncora = ($(linkMenu).offset().top) - 70;
+        jQuery('html, body').animate({scrollTop : valorAncora}, 'slow');
+        return false;
+    });
 
-})();
+    return false;
+
+}());
